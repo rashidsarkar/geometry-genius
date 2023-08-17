7cd97e4a043395e26f6d7d87ae204291929e119c
rashidsarkar <rashidrock558@gmail.com>
2023-08-17 23:56:22 +0600

    comon funtion done

--- js/area.js
+++ js/area.js
@@ -1,30 +1,42 @@
 function calculateTringleArea() {
-  //get base value
-  const baseFild = document.getElementById("traingle-base");
-  const baseValueText = baseFild.value;
-  const base = parseFloat(baseValueText);
-  //get hight value
-  const hightFild = document.getElementById("tringle-height");
-  const hightValueText = hightFild.value;
-  const hight = parseFloat(hightValueText);
+  const base = getInputValue("traingle-base");
+  const hight = getInputValue("tringle-height");
+
   const area = 0.5 * base * hight;
-  console.log(area);
-  //show output
-  const areaSpan = document.getElementById("tringle-aria");
-  areaSpan.innerText = area;
+
+  setElementInnerText("tringle-aria", area);
+  //get base value
 }
 //Rectangle
 function calculateRectangleArea() {
-  const widthFild = document.getElementById("rectangle-width");
-  const widthValuetext = widthFild.value;
-  const width = parseFloat(widthValuetext);
-  console.log(width);
+  const width = getInputValue("rectangle-width");
+
   //get rectangle langth
-  const leangthFild = document.getElementById("rectangle-lenght");
-  const leanghtValueText = leangthFild.value;
-  const leangth = parseFloat(leanghtValueText);
+
+  const leangth = getInputValue("rectangle-lenght");
   const area = width * leangth;
   //show area UI
-  const rectangleAreasSpan = document.getElementById("rectangle-aria");
-  rectangleAreasSpan.innerText = area;
+  setElementInnerText("rectangle-aria", area);
+}
+
+//parallelogram funtion
+function calculateParallelogramArea() {
+  const base = getInputValue("parallelogram-base");
+  const hight = getInputValue("parallelogram-height");
+  const area = base * hight;
+  setElementInnerText("parallelogram-aria", area);
+}
+
+//reduse funtine input in number
+function getInputValue(fildId) {
+  const inpuFild = document.getElementById(fildId);
+  const inputValueText = inpuFild.value;
+  const value = parseFloat(inputValueText);
+  return value;
+}
+
+// reduse set text
+function setElementInnerText(elementId, area) {
+  const element = document.getElementById(elementId);
+  element.innerText = area;
 }
