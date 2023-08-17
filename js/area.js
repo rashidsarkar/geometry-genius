8a8fcf83f5c8a54c92ab997594c41a7bff448a51
rashidsarkar <rashidrock558@gmail.com>
2023-08-17 19:47:03 +0600

    area calucat

---
+++ js/area.js
@@ -0,0 +1,11 @@
+function calculateTringleArea() {
+  //get base value
+  const baseFild = document.getElementById("traingle-base");
+  const baseValueText = baseFild.value;
+  const base = parseFloat(baseValueText);
+  //get hight value
+  const hightFild = document.getElementById("tringle-height");
+  const hightValueText = hightFild.value;
+  const hight = parseFloat(hightValueText);
+  const area=
+}
