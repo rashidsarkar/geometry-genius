ad466e8f7612781b46678b8d66b9f354595b1092
rashidsarkar <rashidrock558@gmail.com>
2023-08-18 01:22:50 +0600

    ses kaj

--- js/area.js
+++ js/area.js
@@ -5,33 +5,47 @@ function calculateTringleArea() {
   const area = 0.5 * base * hight;
 
   setElementInnerText("tringle-aria", area);
+  addToCalculationEntry("traingle", area);
+
   //get base value
 }
 //Rectangle
 function calculateRectangleArea() {
   const width = getInputValue("rectangle-width");
-
-  //get rectangle langth
-
   const leangth = getInputValue("rectangle-lenght");
-  const area = width * leangth;
-  //show area UI
-  setElementInnerText("rectangle-aria", area);
+  if (isNaN(width) || isNaN(leangth)) {
+    alert("please enter a number");
+    return;
+  } else {
+    const area = width * leangth;
+    setElementInnerText("rectangle-aria", area);
+    addToCalculationEntry("rectangle", area);
+  }
 }
 
 //parallelogram funtion
 function calculateParallelogramArea() {
   const base = getInputValue("parallelogram-base");
   const hight = getInputValue("parallelogram-height");
+  if (isNaN(base) || isNaN(hight)) {
+    alert("please enter a number");
+    return;
+  }
   const area = base * hight;
   setElementInnerText("parallelogram-aria", area);
+  // ad to calculation Entry
+  addToCalculationEntry("Perallogram", area);
 }
 //eleps funtion
 function calculatElepsArea() {
   const majorRadius = getInputValue("ellipse-first-radius");
   const minorRadius = getInputValue("ellipse-secend-radius");
+
   const area = 3.14 * majorRadius * minorRadius;
-  setElementInnerText("eleps-area", area);
+  const areaTowDecemal = area.toFixed(2);
+
+  setElementInnerText("eleps-area", areaTowDecemal);
+  addToCalculationEntry("ellipse", area);
 }
 //reduse funtine input in number
 function getInputValue(fildId) {
@@ -45,4 +59,24 @@ function getInputValue(fildId) {
 function setElementInnerText(elementId, area) {
   const element = document.getElementById(elementId);
   element.innerText = area;
+}
+
+//data validation
+/*
+  1. set input type number
+  2. chack type using typeOf
+  3. Nan ki na tar jonno isNaN dea 
+
+*/
+
+// add to calculation entry
+function addToCalculationEntry(areaType, area) {
+  const calculationEntry = document.getElementById("calculation-entry");
+  const p = document.createElement("p");
+  p.classList.add("my-4");
+  const cound = calculationEntry.childElementCount;
+  p.innerHTML = ` ${cound + 1} . ${areaType} ${area} cm<sup>2</sup> 
+  <button class='btn btn-success btn-sm'> Converd</button>
+  `;
+  calculationEntry.appendChild(p);
 }
