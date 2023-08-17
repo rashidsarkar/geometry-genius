4338a6b9749e43d60efad64b9fffd73e16b1638d
rashidsarkar <rashidrock558@gmail.com>
2023-08-17 23:17:07 +0600

    ractangle done

--- js/area.js
+++ js/area.js
@@ -7,5 +7,24 @@ function calculateTringleArea() {
   const hightFild = document.getElementById("tringle-height");
   const hightValueText = hightFild.value;
   const hight = parseFloat(hightValueText);
-  const area=
+  const area = 0.5 * base * hight;
+  console.log(area);
+  //show output
+  const areaSpan = document.getElementById("tringle-aria");
+  areaSpan.innerText = area;
+}
+//Rectangle
+function calculateRectangleArea() {
+  const widthFild = document.getElementById("rectangle-width");
+  const widthValuetext = widthFild.value;
+  const width = parseFloat(widthValuetext);
+  console.log(width);
+  //get rectangle langth
+  const leangthFild = document.getElementById("rectangle-lenght");
+  const leanghtValueText = leangthFild.value;
+  const leangth = parseFloat(leanghtValueText);
+  const area = width * leangth;
+  //show area UI
+  const rectangleAreasSpan = document.getElementById("rectangle-aria");
+  rectangleAreasSpan.innerText = area;
 }
